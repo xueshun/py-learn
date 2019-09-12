f655a418836df8c2146884b9e8d94674fa082db3
xueshun <xueshun1211@163.com>
2019-09-12 18:09:36 +0800

    python 装饰器

--- py-learn/com.xuesan.py/chapter06/context_demo01.py
+++ py-learn/com.xuesan.py/chapter06/context_demo01.py
@@ -2,16 +2,17 @@
 # 上下文
 
 # 未使用上下文，需要抓取异常
-fd = open('name.txt')
-try:
-    for line in fd:
-        print(line)
-finally:
-    fd.close()
-
+# fd = open('name.txt', encoding='UTF-8')
+# try:
+#     for line in fd:
+#         print(line)
+# except Exception as e:
+#     print(e)
+# finally:
+#     fd.close()
 
 # 使用上下文，with会自动抓取异常
 
-with open('name.txt') as f:
+with open('name.txt', encoding='utf-8') as f:
     for line in f:
         print(line)
