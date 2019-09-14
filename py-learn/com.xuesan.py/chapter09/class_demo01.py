a276e02196a9cf47670a2e63ee34002d406c8487
xueshun <xueshun1211@163.com>
2019-09-14 15:32:04 +0800

    类与实例

---
+++ py-learn/com.xuesan.py/chapter09/class_demo01.py
@@ -0,0 +1,26 @@
+# encoding:utf-8
+# 类与示例
+
+# 不使用类
+# user1 = {'name': 'tom', 'hp': 100}
+# user2 = {'name': 'jerry', 'hp': 80}
+#
+#
+# def print_role(rolename):
+#     print('name is %s  ,hp is %s' % (rolename['name'], rolename['hp']))
+#
+#
+# print_role(user1)
+
+
+class Player():  # 定义一个类
+    def __init__(self, name, hp):
+        self.name = name  # 变量被称作属性
+        self.hp = hp
+
+    def print_role(self):  # 定义一个方法
+        print('%s: %s' % (self.name, self.hp))
+
+
+user1 = Player('tom', 100)
+user1.print_role()
